6309b16755fb13a38ccf7c55319d353459dc7f84
ryums12 <rms010712@gmail.com>
2026-06-01 14:41:12 +0900

    branch test

---
+++ src/test/TestManagerTest.java
@@ -0,0 +1,16 @@
+package student;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+ class TestManagerTest {
+	
+        private static TestManager manager;
+        
+        @BeforeAll
+        static void setUp() {
+        	manager = new TestManager();
+        }
+
+  }
